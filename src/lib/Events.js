a13144fdbbd708a5b7174ef18a083b5196d1c9ca
CJ R <cj@null.computer>
2017-06-28 14:07:43 -0600

    Fuuuuuuuuck Timezones

--- src/lib/Events.js
+++ src/lib/Events.js
@@ -3,17 +3,18 @@ import { fetchJSON, postJSON, deleteJSON } from './fetchHelpers';
 const BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000/' : 'https://api.meetup.events/';
 const AUTH_URL = `${BASE_URL}auth/login`;
 const API_URL = `${BASE_URL}api/v1/events`;
+const offset = new Date().getTimezoneOffset() / 60;
 
 function login(password) {
   return postJSON(AUTH_URL, { password });
 }
 
 function getAll() {
-  return fetchJSON(API_URL);
+  return fetchJSON(`${API_URL}?offset=${offset}`);
 }
 
 function getUpcoming() {
-  return fetchJSON(`${API_URL}/upcoming`);
+  return fetchJSON(`${API_URL}/upcoming?offset=${offset}`);
 }
 
 function create(event) {
